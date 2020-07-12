ab9f1901e58c9d396fea187bfb00835748d48d1e
Thakuriprince <princethakuri11111@gmail.com>
2020-07-12 09:24:49 +0545

    update

---
+++ frontend/src/data.js
@@ -0,0 +1,38 @@
+export default   {
+    products:[{
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        name: 'Fit Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 20,
+        brand: 'Nike',
+        rating: 5,
+        numReviews: 10
+    },
+    {
+        name: 'Best Pants',
+        category:'Pants',
+        image: '/images/d1.jpg',
+        price: 30,
+        brand: 'Nike',
+        rating: 4,
+        numReviews: 10
+    },
+    {
+        name: 'Best Pants',
+        category:'Pants',
+        image: '/images/d1.jpg',
+        price: 30,
+        brand: 'Nike',
+        rating: 4,
+        numReviews: 10
+    }]
+}
